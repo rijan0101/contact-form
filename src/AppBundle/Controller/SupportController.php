d1de61bcee47dcc4032dd892de67284e45dd077d
rijan <rijan@crossovernepal.com>
2017-05-28 16:00:55 +0545

    added form

---
+++ src/AppBundle/Controller/SupportController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
+
+class SupportController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('message', TextareaType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            dump($form->getData());
+        }
+        return $this->render('support/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
